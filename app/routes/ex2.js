6175c2d00957864012350c9eeb8be1da6fb28882
jcheron <myaddressmail@gmail.com>
2020-02-08 00:33:56 +0100

    Create Services class

--- app/routes/ex2.js
+++ app/routes/ex2.js
@@ -2,6 +2,24 @@ import Route from '@ember/routing/route';
 import Services from 'ember-tds/utils/services-class';
 export default Route.extend({
   model(){
-    return Services.create();
+    return Services.create({services:[
+        {
+          "name": "Web Development",
+          "price": 300,
+          "active":true
+        },{
+          "name": "Design",
+          "price": 400,
+          "active":false
+        },{
+          "name": "Integration",
+          "price": 250,
+          "active":false
+        },{
+          "name": "Formation",
+          "price": 220,
+          "active":false
+        }
+      ]});
   }
 });
--- app/utils/services-class.js
+++ app/utils/services-class.js
@@ -1,4 +1,10 @@
-import EmberObject,{computed} from '@ember/object';
+import EmberObject,{get,computed} from '@ember/object';
 export default EmberObject.extend({
-
+  countActive:computed('services.@each.active',function(){
+    return this.get('services').filterBy('active', true).length;
+  }),
+  sumActive:computed('services.@each.active',function(){
+    let services = this.get('services').filterBy('active', true);
+    return services.reduce((total,service)=>total+get(service,'price'),0);
+  }),
 });
