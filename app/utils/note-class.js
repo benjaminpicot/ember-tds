f708a3b8eed32a34770b1c2fcc6e0c854d7bb5aa
jcheron <myaddressmail@gmail.com>
2020-02-07 20:02:17 +0100

    Add computed properties

--- app/utils/note-class.js
+++ app/utils/note-class.js
@@ -3,12 +3,24 @@ export default EmberObject.extend({
   MAX:100,
   content:'',
   info:'',
-  style:'',
-  modif:false,
   size:computed('content',function(){
-    return this.get('MAX')-(this.get('content').length);
+    let content=this.get('content');
+    if(content!==''){
+      this.set('info','Note modifiée');
+    }
+    return this.get('MAX')-content.length;
+  }),
+  style:computed('size',function(){
+    let size=this.get('size');
+    if(size<20){
+      return 'danger';
+    }
+    if(size<50){
+      return 'warning';
+    }
+    return 'info';
   }),
   alertVisible:computed('info',function(){
-    return this.get('info').length !=0;
+    return this.get('info').length !==0;
   })
 });
