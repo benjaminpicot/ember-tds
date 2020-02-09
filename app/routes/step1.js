96704329134bf5c450ad37ce0bcbc96f451d4797
jcheron <myaddressmail@gmail.com>
2020-02-09 14:39:35 +0100

    add action addTo

--- app/routes/step1.js
+++ app/routes/step1.js
@@ -1,8 +1,19 @@
 import Route from '@ember/routing/route';
 import Services from 'ember-tds/utils/services-class';
 import datas from 'ember-tds/utils/services-datas';
+import {set} from '@ember/object';
+
 export default Route.extend({
   model(){
     return Services.create(datas);
   },
+  actions:{
+    addTo:function(source,what,dest){
+      dest.pushObjects(what);
+      source.removeObjects(what);
+      let model=this.modelFor(this.routeName);
+      set(model,'selectedDispoItemsIds',[]);
+      set(model,'selectedIncludedItemsIds',[]);
+    },
+  }
 });
