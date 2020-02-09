37efde55fbbaee68db91a3696acecddb893308d3
jcheron <myaddressmail@gmail.com>
2020-02-09 14:31:35 +0100

    Add services-class

---
+++ app/utils/services-class.js
@@ -0,0 +1,22 @@
+// services-class.js
+import EmberObject, { computed } from '@ember/object';
+
+export default EmberObject.extend({
+  init(params){
+    this._super(...arguments);
+    this.set('dispoItems',params);
+    this.set('includedItems',[]);
+    this.set('selectedDispoItemsIds',[]);
+    this.set('selectedIncludedItemsIds',[]);
+  },
+  selectedDispoItems: computed('selectedDispoItemsIds.[]','dispoItems.[]', function() {
+    return this.get('selectedDispoItemsIds').map((id) => {
+      return this.get('dispoItems').findBy('id', id);
+    });
+  }),
+  selectedIncludedItems: computed('selectedIncludedItemsIds.[]','includedItems.[]', function() {
+    return this.get('selectedIncludedItemsIds').map((id) => {
+      return this.get('includedItems').findBy('id', id);
+    });
+  })
+});
