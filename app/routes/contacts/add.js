9fe75b8e37f86b17132a32daff0d276c0050369a
jcheron <myaddressmail@gmail.com>
2020-03-03 15:40:08 +0100

    Update add.js

--- app/routes/contacts/add.js
+++ app/routes/contacts/add.js
@@ -3,7 +3,7 @@ import EmberObject from '@ember/object';
 
 export default Route.extend({
   model(){
-    return EmberObject.create({nom:'SMITH'});
+    return EmberObject.create();
   },
   actions:{
     save:function(datas){
